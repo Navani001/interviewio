669b3de7403ca03517cde7757fac89ded7c13185
navani_hk <navaneetha2006krishnan@gmail.com>
2025-05-28 18:18:10 +0530

    error fix

--- src/lib/Debug.tsx
+++ src/lib/Debug.tsx
@@ -17,7 +17,7 @@ export const useDebugMode = ({ logLevel }: { logLevel?: LogLevel }) => {
       console.log('setting up datadog logs');
       datadogLogs.init({
         clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
-        site: process.env.NEXT_PUBLIC_DATADOG_SITE,
+        site: (process.env.NEXT_PUBLIC_DATADOG_SITE || `datadoghq.com`) as any,
         forwardErrorsToLogs: true,
         sessionSampleRate: 100,
       });
